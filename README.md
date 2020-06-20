6d520a5c9f482d40c90cd50fd6fb17afdbd1929f
sportshead <32637656+sportshead@users.noreply.github.com>
2020-06-20 14:40:55 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# hypixel thing
+
+## commands
+- `yarn` install deps
+- `yarn start` start server at http://localhost:3000
+- `yarn build` build
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Thing from "./components/Thing.jsx"
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>Thing</h1>
+			</header>
+			<Thing />
+		</div>
+	);
+}
+
+export default App;
