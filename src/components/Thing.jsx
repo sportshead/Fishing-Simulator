0fc89ca2e559c272935a325631d5a9c0e0567cd1
sportshead <32637656+sportshead@users.noreply.github.com>
2020-06-22 10:40:25 +0800

    Fix CSS

--- src/components/Thing.jsx
+++ src/components/Thing.jsx
@@ -146,8 +146,8 @@ export default class Thing extends React.Component {
 						style={{
 							position: "absolute",
 							left: "50%",
-							top: "50%",
 							transform: "translate(-50%, -50%)",
+							transform: "translate(-50%, 0)",
 						}}
 					>
 						<tr>
@@ -203,10 +203,10 @@ export default class Thing extends React.Component {
 							<td>{data.lootLuck}</td>
 						</tr>
 						<tr>
-							<th>
-								<h3>
+							<th colspan="2">
+								<h2>
 									<b>Equipment</b>
-								</h3>
+								</h2>
 							</th>
 						</tr>
 						<tr>
